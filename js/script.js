be37ec632d981a6f9009e99722af3540f0cee5e9
Melisha <mslafaber1@gmail.com>
2020-09-12 17:04:39 +0930

    Added all the relevant pages for each emergency type their own emergency contact number

--- js/script.js
+++ js/script.js
@@ -12,8 +12,24 @@ for(let i = 0; i < buttons.length; i++) {
 } */
 function pageRedirectfor000() {
     window.location.href = "000contact.html";
-  }  
+  }
 
-function pageRedirecttohome() {
-    window.location.href = "index.html";
+function pageRedirectforpoisons() {
+    window.location.href = "poisons.html";
+}
+
+function pageRedirectfordisaster() {
+    window.location.href = "disaster.html";
+  }
+
+function pageRedirectformentalhealth() {
+    window.location.href = "mentalhealth.html";
+}
+
+function pageRedirectforcrime() {
+    window.location.href = "crime.html";
+  }
+
+function pageRedirectforunsuremedical() {
+    window.location.href = "unsuremedical.html";
 }
