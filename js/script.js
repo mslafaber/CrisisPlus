bf38e94b763cfdde6f11e391481dda85caccf6fe
Melisha <mslafaber1@gmail.com>
2020-09-13 09:30:58 +0930

    Deleted the manifest,json and sw.js files since making the app progressive and installable was not successful.

--- js/script.js
+++ js/script.js
@@ -1,13 +1,3 @@
-if ("serviceWorker" in navigator) {
-    navigator.serviceWorker.register("sw.js").then(registration => {
-        console.log("SW Registered");
-        console.log(registration);
-    }).catch(error => {
-        console.log("SW Registration Failed!");
-        console.log(error); 
-    });
-}
-
 function pageRedirectfor000() {
     window.location.href = "000contact.html";
   }
